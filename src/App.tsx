784ffa4448ecae0f69eb743aad15a2f53f34bf70
Juan Tellez <jmtp12poke@gmail.com>
2026-04-08 22:46:46 -0600

    feat(*): init commit for profile app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Profiles</h1>        
+      </section>
+
+    </>
+  )
+}
+
+export default App
